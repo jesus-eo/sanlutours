de076c8833adfa74c6a6f427266deefffdc9733e
jesus-eo <jesusmanuel.espinar@iesdonana.org>
2022-05-08 09:37:10 +0200

    Commit inicial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,20 +18,20 @@
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="flex">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100 w-full flex">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
-            @if (isset($header))
+            {{-- @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endif
+            @endif --}}
 
             <!-- Page Content -->
             <main>
