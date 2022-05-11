cd13bf92bff25ddfeb44113adb8c865f2525fd76
jesus-eo <jesusmanuel.espinar@iesdonana.org>
2022-05-11 21:29:30 +0200

    No funciona geolocalizacion

--- resources/views/livewire/filtros.blade.php
+++ resources/views/livewire/filtros.blade.php
@@ -31,10 +31,10 @@
                 </div>
             </div>
             {{-- Busqueda --}}
-            <div class="pt-2 relative mr-auto ml-4 text-gray-600">
+            <div class="pt-2 relative ml-4 mr-4 text-gray-600">
                 <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg  focus:outline-none"
                     type="search" name="search" placeholder="Search" wire:model="busqueda">
-                <button class="lupa absolute right-0 left-5 top-0 mt-5 mr-4">
+                <button class="lupa absolute right-0  top-0 mt-5 mr-4">
                     <svg class="text-gray-600 h-4 w-4 fill-current" version="1.1" id="Capa_1" x="0px" y="0px"
                         viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" width="512px"
                         height="512px">
